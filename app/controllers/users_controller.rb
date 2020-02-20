3bd55e4f5dd45f8c70e999f8abfd4e318bfb6b8f
Deanna Kelley <deanna.georges@gmail.com>
2020-02-20 18:56:37 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    erb :index
+  end
+
+  get '/signup' do
+    # if Helpers.is_logged_in?(session)
+    #   redirect to '/tweets'
+    # end
+
+    erb :"/users/create_user"
+  end
+
 
 end
